74a24966077ab7faa9b0ab1cedc859d62c5c7184
Samuel Elvis <169075141+osamuelelvis@users.noreply.github.com>
2026-03-04 11:31:08 -0300

    Update Main.kt

--- 007NoName/src/main/kotlin/Main.kt
+++ 007NoName/src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+// 7 - Escreva um algoritmo para ler as notas de avaliações de um aluno, calcule e imprima a média (simples) desse aluno. Só devem ser aceitos valores válidos durante a leitura (0 a 10) para cada nota. São 6 notas ao total.
+// Caso o valor informado para qualquer uma das notas esteja fora do limite estabelecido, deve ser solicitado um novo valor ao usuário.
 fun main() {
     var numero: Float
     var soma = 0.0

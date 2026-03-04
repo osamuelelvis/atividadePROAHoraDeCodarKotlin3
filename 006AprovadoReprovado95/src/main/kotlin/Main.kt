1ef2c4f3b0ae638321aa6e5b72aded86b1c11970
Samuel Elvis <169075141+osamuelelvis@users.noreply.github.com>
2026-03-04 11:30:38 -0300

    Update Main.kt

--- 006AprovadoReprovado95/src/main/kotlin/Main.kt
+++ 006AprovadoReprovado95/src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+// 6 - Escreva um programa para ler 2 notas de um aluno, calcular e imprimir a média final. Considere que a nota de aprovação é 9,5. Logo após escrever a mensagem "Calcular a média de outro aluno Sim/Não?" e solicitar um resposta. Se a resposta for "S", o programa deve ser executado novamente, caso contrário deve ser encerrado exibindo a quantidade de alunos aprovados.
 fun main() {
     var nota1: Double
     var nota2: Double

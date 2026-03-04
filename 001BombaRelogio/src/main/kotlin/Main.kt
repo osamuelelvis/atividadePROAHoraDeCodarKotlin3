f3c76c6e7f292b8df26500890ea2608a53546a7c
Samuel Elvis <169075141+osamuelelvis@users.noreply.github.com>
2026-03-04 11:29:04 -0300

    Update Main.kt

--- 001BombaRelogio/src/main/kotlin/Main.kt
+++ 001BombaRelogio/src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+// 1 - Crie uma bomba relógio (usando somente código - para deixar claro!) cuja contagem regressiva vá de 30 a 0. No final da repetição escreva "EXPLOSÃO".
 fun main() {
     println("Uma bomba relógio foi acionada! Ela vai explodir em 30 segundos!")
     for (c in 30 downTo 0) {
